9d1c1aa0b435ccc58c272c80e23cc2f23c7c40ef
DAST-Macbook <jackie@dast.tw>
2023-07-30 13:35:50 +0800

    style: update input prompt and year conversion methods
    - Change the input prompt for name and year to use single quotes instead of double quotes
    - Change the way the year is converted to an integer
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- homework.py
+++ homework.py
@@ -1,7 +1,8 @@
-name = input("請輸入姓名：")
-years = input("請輸入年齡：")
-year = int(years)
+name = input('請輸入姓名：')
+year = int(input('請輸入年齡'))
+
 workyears = 60 - year
+
 if year >= 65:
     print('您的年齡已達退休年齡')
 else:
