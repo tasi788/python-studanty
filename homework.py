6aa758c847eecaea9c8a844d7d7f6d147ac16046
DAST-Macbook <jackie@dast.tw>
2023-07-30 12:00:10 +0800

    refactor: change variable `years` from float to integer
    - Change the variable `years` from a float to an integer
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
 name = input("請輸入姓名：")
 years = input("請輸入年齡：")
-workyears = 60 - float(years)
-print(f'姓名', {name}, '還有', {workyears}, '年退休')
+workyears = 60 - int(years)
+print(f'姓名', name, '還有', workyears, '年退休')
