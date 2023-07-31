4b54166f4f3ca0c8e6b16750684cc04cecbe965a
DAST-Macbook <jackie@dast.tw>
2023-07-31 19:17:54 +0800

    ### 重構：更改退休年齡條件並刪除檔案結尾的換行符號
    - 將退休年齡條件改為使用 `工作年數` 而非 65
    - 刪除檔案結尾的換行符號
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>
    
    ###

--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@ age = int(input('請輸入年齡:'))
 workyears = 65      #法定退休年齡
 years = workyears - age     #自動減去退休年齡
 
-if age >= workyears:    #限定年齡大於65歲自動顯示已退休
+if age >= workyears:    #法定退休年齡減去目前年齡
     print('您的年齡已達退休年齡')
 else:
     print(f'姓名', name, '還有', years, '年退休')
