fb9b3356eacb1d9bb211232763fdcb7737b34910
HomePC-DAST <jackie@dast.tw>
2023-08-01 22:20:08 +0800

    refactor: refactor code for improved clarity and readability
    - Change the print statement to include double quotation marks
    - Add an empty line for better readability
    - Remove the unnecessary code for inputting name
    - Rename the variable `years` to better describe its purpose
    - Add a comment to clarify the purpose of the code section
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- homework-def.py
+++ homework-def.py
@@ -3,7 +3,8 @@
 
 import time as t   #載入time模組，並取名為t
 
-print('本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算')
+print('「本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算」')
+
 workyears = 65    # 設定法定退休年齡
 name = input('請輸入姓名:')
 # 要求輸入年齡的資料必須為數字，若不是數字則返回要求重新輸入年齡
@@ -13,12 +14,13 @@ while True:
         break
     except ValueError:
         print('請重新輸入正確數值，年齡需為數字')
+        continue
 # 設定目前年齡若大於等於法定退休年齡直接顯示已達退休年齡，若未達法定退休年齡則顯示計算後年數
 def numbers(years):
     if age >= workyears:
         print('您的年齡已達退休年齡')
     else:
-        print(f'姓名', name, '還有', workyears - years, '年退休')
+        print(f'姓名', name, '您還有', workyears - years, '年即將退休')
 Value = numbers(age)
 
 print('【5秒後自動退出程式】')
