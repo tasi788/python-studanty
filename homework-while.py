bdb788ef64ed41b8903ec1d81c869b1a5fc8b279
DAST-OfficePC <jackie@dast.tw>
2023-08-01 13:20:35 +0800

    feat: add time module import and exit pauses in homework scripts
    - Add import statement for the `time` module in `homework-input.py`
    - Add import statement for the `time` module in `homework-while.py`
    - Add a 10-second pause before exiting the program in `homework-input.py`
    - Add a 10-second pause before exiting the program in `homework-while.py`
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- homework-while.py
+++ homework-while.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # 本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算
 
+import time    #載入time模組
+
 name = input('請輸入姓名:')
 
 # 要求輸入年齡的資料必須為數字，若不是數字則返回要求重新輸入年齡
@@ -19,3 +21,5 @@ if age >= workyears:
     print('您的年齡已達退休年齡')
 else:
     print(f'姓名', name, '還有', years, '年退休')
+
+time.sleep(10)    #停留10秒後退出程式
