83571ea128da866d72e8a455251b9e09a43a287c
DAST-Macbook <jackie@dast.tw>
2023-08-01 18:13:42 +0800

    style: update print and error messages, add new line at end of file
    - Modify the print statement to include double quotes around the message
    - Change the error message to include "結束" at the end
    - Add a new line at the end of the file
    - Modify the print statement to include double quotes around the message
    - Change the error message to include "結束" at the end
    - Add a new line at the end of the file
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- homework-input.py
+++ homework-input.py
@@ -4,7 +4,7 @@
 import sys as s   #載入sys模組，並取名為s
 import time as t   #載入time模組，並取名為t
 
-print('本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算')
+print('「本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算」')
 name = input('請輸入姓名:')
 
 # 要求輸入年齡的資料必須為數字，若不是數字則返回錯誤訊息並退出
@@ -13,7 +13,7 @@ try:
     age = int(input('請輸入年齡：'))
 except ValueError:
     print('請重新執行程式，輸入年齡需為數字')
-    print('【5秒後自動退出程式】')
+    print('【5秒後自動退出結束程式】')
     t.sleep(5)    # 停留5秒後退出程式
     s.exit(1)
 
@@ -26,5 +26,5 @@ if age >= workyears:
 else:
     print(f'姓名', name, '還有', years, '年退休')
 
-print('【5秒後自動退出程式】')
+print('【5秒後自動退出結束程式】')
 t.sleep(5)    # 停留5秒後退出程式
