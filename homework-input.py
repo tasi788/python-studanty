4c2bde8b658f9d4cce9149693c1bd0e843f578b8
DAST-OfficePC <jackie@dast.tw>
2023-08-01 13:30:12 +0800

    refactor: refactor file imports and variable names in homework files
    - Rename `sys` to `s` and `time` to `t` in `homework-input.py`
    - Remove the unused `import sys` line in `homework-input.py`
    - Remove the unused `import time` line in `homework-while.py`
    - Remove the `time.sleep(10)` line in `homework-while.py`
    
    Signed-off-by: DAST-OfficePC <jackie@dast.tw>

--- homework-input.py
+++ homework-input.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # 本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算
 
-import sys    #載入sys模組
-import time    #載入time模組
+import sys as s   #載入sys模組，並取名為s
+import time as t   #載入time模組，並取名為t
 
 name = input('請輸入姓名:')
 
@@ -12,10 +12,11 @@ try:
     age = int(input('請輸入年齡：'))
 except ValueError:
     print('請重新執行程式，輸入年齡需為數字')
-    sys.exit(1)
+    t.sleep(5)    # 停留5秒後退出程式
+    s.exit(1)
 
-workyears = 65    #設定法定退休年齡
-years = workyears - age    #法定退休年齡減去目前年齡
+workyears = 65    # 設定法定退休年齡
+years = workyears - age    # 法定退休年齡減去目前年齡
 
 # 設定目前年齡若大於等於法定退休年齡直接顯示已達退休年齡，若未達法定退休年齡則顯示計算後年數
 if age >= workyears:
@@ -23,4 +24,4 @@ if age >= workyears:
 else:
     print(f'姓名', name, '還有', years, '年退休')
 
-time.sleep(10)    #停留10秒後退出程式
+t.sleep(5)    # 停留5秒後退出程式
