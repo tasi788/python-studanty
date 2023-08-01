d2e97a648239394b56e46fcfd1c5d754052fd8ee
HomePC-DAST <jackie@dast.tw>
2023-08-01 21:53:47 +0800

    feat: fix retirement age calculation and remove unnecessary code
    - Remove unnecessary code related to checking retirement age
    - Fix a typo in the comment
    - Modify the `numbers` function to correctly calculate years until retirement
    - Remove unused variable `Value`
    
    Signed-off-by: HomePC-DAST <jackie@dast.tw>

--- homework-def.py
+++ homework-def.py
@@ -6,13 +6,6 @@ import time as t   #載入time模組，並取名為t
 print('本程式內容為輸入姓名及年齡後進行法定退休年齡自動計算')
 workyears = 65    # 設定法定退休年齡
 name = input('請輸入姓名:')
-
-# 設定目前年齡若大於等於法定退休年齡直接顯示已達退休年齡，若未達法定退休年齡則顯示計算後年數
-def numbers(years):
-    if workyears - age >= workyears:
-        print('您的年齡已達退休年齡')
-    else:
-        print(f'姓名', name, '還有', years, '年退休')
 # 要求輸入年齡的資料必須為數字，若不是數字則返回要求重新輸入年齡
 while True:
     try:
@@ -20,6 +13,13 @@ while True:
         break
     except ValueError:
         print('請重新輸入正確數值，年齡需為數字')
+# 設定目前年齡若大於等於法定退休年齡直接顯示已達退休年齡，若未達法定退休年齡則顯示計算後年數
+def numbers(years):
+    if age >= workyears:
+        print('您的年齡已達退休年齡')
+    else:
+        print(f'姓名', name, '還有', workyears - years, '年退休')
 Value = numbers(age)
+
 print('【5秒後自動退出程式】')
 t.sleep(5)    # 停留5秒後退出程式
