812e28c31c5ea9e3dd29f821a030cab99bdb77f6
DAST-Macbook <jackie@dast.tw>
2023-08-06 15:29:13 +0800

    feat: implement rotation of elements in a list
    - Add a new file `homework-sort.py`
    - Initialize a list `x` with 10 elements
    - Perform a rotation of the elements in the list `x`
    - Print the updated list `x`
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

---
+++ homework-sort.py
@@ -0,0 +1,12 @@
+#!/bin/env python3
+
+x = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10']
+
+n = 3
+while n > 0:
+    y = x[-1]
+    x.insert(0, y)
+    del x[-1]
+    n = n - 1
+
+print(x)
