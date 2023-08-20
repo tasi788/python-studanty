c186614ef67b7184ff7d6133f504ad10369072bc
DAST-Macbook <jackie@dast.tw>
2023-08-20 15:25:01 +0800

    refactor: refactor circle calculation logic
    - Remove unnecessary code and comments
    - Import the `mymath` module
    - Change the print statements to use the `mymath` module functions
    - Remove the unused variables and loop
    - Prompt the user for input and calculate the circle's circumference and area
    - Add missing new line at the end of the file
    
    Signed-off-by: DAST-Macbook <jackie@dast.tw>

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python3
+import mymath
 
-print ('hello, world')
-print ('I\'m DAST')
-      
-n = 9
-r = 1
-while n > 0:
-    r = r * n
-    n = n - 1
-print(r)
-
-input = input('請輸入圓半徑數值:')
-radius = float(input)
-PI = 3.14
-print('圓半徑：', radius)
-print('圓周長：', 2 * PI * radius)
-print('圓面積：', PI * radius * radius)
+print(mymath.pi)
+print(mymath.area(2))
